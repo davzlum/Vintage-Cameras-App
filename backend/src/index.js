27ae23f32516fe141b6c613447051303cc813ab9
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-31 17:10:17 +0200

    Configure morgan and cors for database state terminal

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const cors = require('cors');
+const morgan = require('morgan');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 require('./ddbb/mongoose.config');
 
 const camerasRoute = require('./routes/camerasRoute');
@@ -17,4 +22,5 @@ const usersRoute = require('./routes/usersRoute');
 
 app.use('/users', usersRoute);
 
-app.listen(port, debug(`Server is running in ${chalk.bgCyan(port)}`));
+app.listen(port,
+  () => debug(`Server is running in ${chalk.magentaBright(`localhost:${port}`)}`));
