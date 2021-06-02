3ecee08138190944743b1aaa697cef14bab86985
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-06-02 10:44:08 +0200

    Create header component and add styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import CamerasList from './components/cameras/CamerasList';
+import Header from './components/common/Header';
 import configureStore from './redux/store';
 import './App.css';
 
 function App() {
   return (
     <Provider store={configureStore()}>
-      <h1>Hola</h1>
+      <Header />
       <CamerasList />
     </Provider>
   );
---
+++ frontend/src/components/common/Header/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './header.css';
+import logo from '../../../assets/logo.svg';
+import cart from '../../../assets/shopping-cart-solid.svg';
+import user from '../../../assets/user-solid.svg';
+import nav from '../../../assets/bars-solid.svg';
+
+function Header() {
+  return (
+    <header className="header">
+      <ul className="header-container">
+        <li className="header-container__nav">
+          <img src={nav} alt="navigation" />
+          <ul className="sections-list">
+            <li className="section-item">Cameras</li>
+            <li className="section-item">Lenses</li>
+            <li className="section-item">Films</li>
+          </ul>
+        </li>
+        <li className="header-container__logo">
+          <img src={logo} alt="logo" />
+        </li>
+        <li className="header-container__right">
+          <span className="header-cart">
+            <img src={cart} alt="cart" />
+          </span>
+          <span className="header-user">
+            <img src={user} alt="user" />
+          </span>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
