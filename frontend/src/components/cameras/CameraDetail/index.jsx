47301ebb9d9303c780acb07ac4da1c8bb58a349d
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-06-02 19:51:42 +0200

    Install slider library and configurate with detail images

--- frontend/src/components/cameras/CameraDetail/index.jsx
+++ frontend/src/components/cameras/CameraDetail/index.jsx
@@ -1,16 +1,24 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
+import Slider from 'react-slick';
 import { loadProduct } from '../../../redux/actions/actionCreators';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './cameraDetail.scss';
 
 function CameraDetail({ selectedProduct, dispatch }) {
   const { cameraId } = useParams();
   useEffect(() => {
     dispatch(loadProduct(cameraId, 'cameras'));
   }, []);
+  const renderSlides = () => selectedProduct?.images?.map((img) => <div className="img-container"><img src={img} alt="images" /></div>);
+
   return (
     <div>
+      <Slider dots>{renderSlides()}</Slider>
       <h1>{selectedProduct?.cameraModel}</h1>
     </div>
   );
@@ -19,6 +27,7 @@ function CameraDetail({ selectedProduct, dispatch }) {
 CameraDetail.propTypes = {
   selectedProduct: PropTypes.shape({
     cameraModel: PropTypes.string,
+    images: PropTypes.shape([]).isRequired,
   }).isRequired,
   dispatch: PropTypes.func.isRequired,
 };
