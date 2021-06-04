763d31eabd54202df397b9f1536419aa427e407e
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-06-04 15:51:16 +0200

    Solve problem with enviroment url login

--- frontend/src/redux/actions/actionCreatorsUser.js
+++ frontend/src/redux/actions/actionCreatorsUser.js
@@ -2,12 +2,12 @@
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
-// const url = process.env.REACT_APP_URL_LOGIN;
+const urlLogin = process.env.REACT_APP_LOGIN_URL;
 
 export default function login(email, password) {
   return async (dispatch) => {
     try {
-      const { data } = await axios.post('http://localhost:2021/login', { email, password });
+      const { data } = await axios.post(urlLogin, { email, password });
       dispatch({
         type: actionTypes.LOGIN,
         user: data,
