5e300367dc8c3d9e235a5f3485b1773e342d44d8
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-06-07 12:12:51 +0200

    Apply redux to favorites, add action creators and reducer with add and delete product from favorite

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import productsReducer from './productsReducer';
 import cartProductsReducer from './cartProductsReducer';
 import selectedProductReducer from './selectedProductReducer';
 import usersReducer from './usersReducer';
+import favoritesReducer from './favoritesProductsReducer';
 
 const rootReducer = combineReducers({
   products: productsReducer,
   selectedProduct: selectedProductReducer,
   cartList: cartProductsReducer,
   user: usersReducer,
+  favorites: favoritesReducer,
 });
 
 export default rootReducer;
