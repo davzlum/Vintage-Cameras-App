9abc6add0173c1e2e1ebf94f8b8571b6763f1576
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-06-09 10:49:16 +0200

    Register component take data and save it with useState

--- frontend/src/components/Register/index.jsx
+++ frontend/src/components/Register/index.jsx
@@ -1,63 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Register() {
+  const dispatch = useDispatch();
+  const [data, setData] = useState({
+    name: '',
+    username: '',
+    email: '',
+    address: '',
+    city: '',
+    cp: '',
+    phone: 0,
+    password: '',
+    favorites: [],
+    cart: [],
+  });
+  const handleInputChange = (event) => {
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const sendData = (event) => {
+    event.preventDefault();
+    dispatch();
+  };
+
   return (
     <>
       <h1>Register</h1>
-      <form className="form-container">
+      <form className="form-container" onSubmit={sendData}>
         <p>(*) is required</p>
         <div className="form-floating">
           <label htmlFor="name">
             <p>Name: *</p>
-            <input type="text" placeholder="Insert name" required />
+            <input type="text" placeholder="Insert name" name="name" required onChange={handleInputChange} />
           </label>
         </div>
         <div className="form-floating">
           <label htmlFor="username">
             <p>Username: *</p>
-            <input type="text" placeholder="Insert username" required />
+            <input type="text" placeholder="Insert username" name="username" required onChange={handleInputChange} />
           </label>
         </div>
         <div className="form-floating">
           <label htmlFor="email">
             <p>Email: *</p>
-            <input type="email" placeholder="Insert email" required />
+            <input type="email" placeholder="Insert email" name="email" required onChange={handleInputChange} />
           </label>
         </div>
         <div className="form-floating">
           <label htmlFor="password">
             <p>Password: *</p>
-            <input type="password" placeholder="Insert password" minLength="6" required />
+            <input type="password" placeholder="Insert password" name="password" minLength="6" required onChange={handleInputChange} />
           </label>
         </div>
         <div className="form-floating">
           <label htmlFor="phone">
             <p>Phone:</p>
-            <input type="tel" placeholder="Insert phone" />
+            <input type="tel" placeholder="Insert phone" name="phone" onChange={handleInputChange} />
           </label>
         </div>
         <div className="form-floating">
           <label htmlFor="address">
             <p>Address:</p>
-            <input type="text" placeholder="Insert address" />
+            <input type="text" placeholder="Insert address" name="address" onChange={handleInputChange} />
           </label>
         </div>
         <div className="form-floating">
           <label htmlFor="city">
             <p>City:</p>
-            <input type="text" placeholder="Insert city" />
+            <input type="text" placeholder="Insert city" name="city" onChange={handleInputChange} />
           </label>
         </div>
         <div className="form-floating">
           <label htmlFor="postal">
             <p>P.C.:</p>
-            <input type="number" placeholder="Insert postal code" maxLength="5" />
+            <input type="number" placeholder="Insert postal code" name="cp" maxLength="5" onChange={handleInputChange} />
           </label>
         </div>
         <div className="button">
           <input className="button__item button__item--black" type="submit" name="sign-button" id="sign-button" value="sign up" />
         </div>
       </form>
+      <ul>
+        <li>{data.name}</li>
+        <li>{data.address}</li>
+      </ul>
     </>
   );
 }
